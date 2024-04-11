5ef1d1189bcb3e04297a70ee69950a7ff61838a6
osunxn <jamesdavids201@gmail.com>
2024-04-11 09:41:07 +0000

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-'''
-This module contains a function to retrieve the number of subscribers for a given subreddit.
-'''
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit does not exist or an error occurs, returns 0.
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-if __name__ == "__main__":
-    # Retrieve the subreddit name from command-line arguments
-    subreddit_name = argv[1]
-
-    # Call the function and print the number of subscribers
-    subscribers = number_of_subscribers(subreddit_name)
-    if subscribers:
-        print("Number of subscribers for r/{}: {}".format(subreddit_name, subscribers))
-    else:
-        print("Subreddit r/{} does not exist or an error occurred.".format(subreddit_name))
+    results = response.json().get("data")
+    return results.get("subscribers")
