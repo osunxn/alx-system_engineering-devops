c072fb1d31bbc5b31ff5a37911e9cad508e65871
osunxn <jamesdavids201@gmail.com>
2024-04-14 15:02:34 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ If an invalid subreddit is given, the function should return 0
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API
@@ -19,6 +18,7 @@ def number_of_subscribers(subreddit):
     )
 
     if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
-        return 0
+        data = req.json().get("data", None)
+        if data is not None:
+            return data.get("subscribers", 0)
+    return 0
