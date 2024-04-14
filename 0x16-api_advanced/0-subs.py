439ef8a1bf39e19c501431c853f27262fd81f925
osunxn <jamesdavids201@gmail.com>
2024-04-14 14:59:40 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
 """
 
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-
-    - If not a valid subreddit, return None.
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
     """
     req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
-        params={"after": after},
     )
 
     if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            hot_list.append(title)
-        after = req.json().get("data").get("after")
-
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
+        return req.json().get("data").get("subscribers")
     else:
-        return None
+        return 0
