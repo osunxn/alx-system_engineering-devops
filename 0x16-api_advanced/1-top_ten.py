4a875d295434d642c597363b1258cbb9df41af54
osunxn <jamesdavids201@gmail.com>
2024-04-14 14:41:44 +0000

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
+u#!/usr/bin/python3
 """
 Function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
-import sys
 
 
 def top_ten(subreddit):
@@ -13,23 +12,25 @@ def top_ten(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, print None.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Custom"}
-    params = {"limit": 10}
+    try:
+        req = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json",
+            headers={"User-Agent": "Custom"},
+            params={"limit": 10},
+        )
+        req.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        data = req.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        print("None")
 
-    req = requests.get(url, headers=headers, params=params)
 
-    if req.status_code == 200:
-        data = req.json().get("data", {}).get("children", [])
-        for post in data:
-            title = post.get("data", {}).get("title")
-            print(title)
-    else:
-        print(None)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 filename.py <subreddit>")
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
+
