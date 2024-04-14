911c1d03c6a7e71dd6283180539bf3f28da534b0
osunxn <jamesdavids201@gmail.com>
2024-04-14 14:40:12 +0000

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -12,16 +13,23 @@ def top_ten(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, print None.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+    params = {"limit": 10}
+
+    req = requests.get(url, headers=headers, params=params)
 
     if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
+        data = req.json().get("data", {}).get("children", [])
+        for post in data:
+            title = post.get("data", {}).get("title")
             print(title)
     else:
         print(None)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 filename.py <subreddit>")
+    else:
+        top_ten(sys.argv[1])
