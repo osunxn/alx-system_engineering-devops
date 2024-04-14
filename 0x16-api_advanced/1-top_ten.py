42cdd0524a5da7532982902b168b6f7289d800cb
osunxn <jamesdavids201@gmail.com>
2024-04-14 14:50:46 +0000

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -12,18 +13,16 @@ def top_ten(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, print None.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "Custom"}
-    params = {"limit": 10}
-
-    req = requests.get(url, headers=headers, params=params)
+    req = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json",
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
     if req.status_code == 200:
-        data = req.json().get("data", {}).get("children", [])
-        for post in data:
-            title = post.get("data", {}).get("title")
+        for get_data in req.json().get("data", {}).get("children", []):
+            dat = get_data.get("data")
+            title = dat.get("title")
             print(title)
-    elif req.status_code == 404:
-        print("None")
     else:
         print(None)
