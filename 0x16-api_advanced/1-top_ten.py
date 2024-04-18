61fc3bc17ec620aafa947c0db97bdafb8d44085d
osunxn <jamesdavids201@gmail.com>
2024-04-18 08:19:50 +0000

    new changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,6 +2,7 @@
 """Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
@@ -13,23 +14,12 @@ def top_ten(subreddit):
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
     if response.status_code == 404:
         print("None")
         return
-
-    # Check if the response content is JSON
     try:
-        data = response.json()
-    except ValueError:  # If not JSON, print None
-        print("None")
-        return
+        results = response.json().get("data")
+        [print(c.get("data").get("title")) for c in results.get("children")]
+    except ValueError:
+        print("Error: Invalid JSON response from Reddit API")
 
-    # Check if the subreddit exists
-    if "error" in data:
-        print("None")
-        return
-
-    # Extract and print titles of the posts
-    for post in data.get("data", {}).get("children", []):
-        print(post.get("data", {}).get("title"))
