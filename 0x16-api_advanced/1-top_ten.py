97d74be0477560bf017999e65f5e0bedb4ec75cd
osunxn <jamesdavids201@gmail.com>
2024-04-21 16:33:59 +0000

    Changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 10 hot posts
-listed for a given subreddit.
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
+    """Print the titles of the 10 hottest posts on a given subreddit.
 
     Args:
-        subreddit: The subreddit to search.
+        subreddit (str): The name of the subreddit to query.
 
     Returns:
-        None if the subreddit is invalid.
+        None: If the subreddit is not valid or there are no posts.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Holberton School"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json().get("data")
-        if data:
-            children = data.get("children")
-            if children:
-                for post in children:
-                    print(post.get("data").get("title"))
-        else:
-            print("No posts found.")
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    results = response.json().get("data")
+    if results and "children" in results:
+        for post in results["children"]:
+            print(post["data"]["title"])
     else:
         print("None")
