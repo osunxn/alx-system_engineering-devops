42900f09e287f27324992f2a5cfa8d3d9acd7499
osunxn <jamesdavids201@gmail.com>
2024-04-24 23:10:33 +0000

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,11 @@
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Or the version that works with Flask (2.1.0)
+package {'werkzeug':
+  ensure   => '2.0.2',
+  provider => 'pip3',
 }
