838de37212b4d4aaa373b990f76acf1e87ee58d1
osunxn <jamesdavids201@gmail.com>
2024-04-24 00:05:17 +0000

    new

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+"""
+
 import requests
 
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+
+    - If not a valid subreddit, return None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+    params = {"after": after}
+
+    try:
+        req = requests.get(url, headers=headers, params=params)
+        req.raise_for_status()  # Raise an error for invalid response status
+
+        data = req.json().get("data")
+        children = data.get("children")
+        after = data.get("after")
+
+        for child in children:
+            hot_list.append(child.get("data").get("title"))
+
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
+        return None
