e398faa5c1821e6dcaac1a0f092bdebb98ce6012
osunxn <jamesdavids201@gmail.com>
2024-04-24 00:27:23 +0000

    new

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,38 +10,23 @@ import requests
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to search.
-
-    Returns:
-        None
+    Print the titles of the 10 hottest posts on a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {"User-Agent": "Custom"}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
 
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an error for invalid response status
-
-        data = response.json().get("data")
-        if not data:
+        results = response.json().get("data")
+        if results:
+            [print(c.get("data").get("title")) for c in results.get("children")[:10]]
+        else:
             print("None")
-            return
-
-        children = data.get("children")
-        if not children:
-            print("None")
-            return
-
-        for i, child in enumerate(children[:10]):
-            title = child.get("data", {}).get("title")
-            if title:
-                print(title)
-            else:
-                print("[No title]")
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    except Exception as e:
         print("None")
