fb82a94f56062a172cded17030b9cfc38af88df7
osunxn <jamesdavids201@gmail.com>
2024-04-26 19:33:42 +0000

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
+1-top_ten
 """
-
 import requests
+
 
 def top_ten(subreddit):
     """
-    Print the titles of the 10 hottest posts on a given subreddit.
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    If the subreddit is invalid, prints None.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print("None")
-        return
-
-    try:
-        results = response.json().get("data")
-        if results:
-            [print(c.get("data").get("title")) for c in results.get("children")[:10]]
-        else:
-            print("None")
-    except Exception as e:
-        print("None")
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            children = data.get('children')
+            if children:
+                for post in children[:10]:
+                    print(post.get('data').get('title'))
+                return
+    print(None)
