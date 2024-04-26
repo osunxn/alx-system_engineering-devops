f94ab0e511ed4f9ec600b0101a0ae197d9af93f1
osunxn <jamesdavids201@gmail.com>
2024-04-26 18:28:58 +0000

    add

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,14 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    If the subreddit is invalid or does not exist, returns 0.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
+        if data and 'subscribers' in data:
+            return data['subscribers']
     return 0
