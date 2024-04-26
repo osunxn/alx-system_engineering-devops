8a3c8b8e4e01c9074abcb35972047f132c7dcf33
osunxn <jamesdavids201@gmail.com>
2024-04-26 19:48:37 +0000

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
-def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    try:
-        data = response.json().get('data')
-        if response.status_code == 200 and data:
-            children = data.get('children')
-            if children:
-                for post in children[:10]:
-                    print(post.get('data').get('title'))
-                return
-    except ValueError:  # JSONDecodeError in Python 3.5 and earlier
-        pass  # Print None if there's an issue with JSON parsing
-    print(None)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
